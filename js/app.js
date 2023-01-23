1327e74a775d05c78af50d01caa7a41c171cc3ae
zoheir kabuli <zoheyrk@gmail.com>
2023-01-24 02:08:41 +0330

    complete form and fix some style

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
-const homeHeroSlider = new Swiper(".home-hero-slider", {
-  spaceBetween: 20,
-  loop: true,
-  pagination: {
-    el: ".hero-slider-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".hero-slider-button-next",
-    prevEl: ".hero-slider-button-prev",
-  },
-});
+if (document.querySelector(".home-hero-slider")) {
+  const homeHeroSlider = new Swiper(".home-hero-slider", {
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+      el: ".hero-slider-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".hero-slider-button-next",
+      prevEl: ".hero-slider-button-prev",
+    },
+  });
+}
+
+const inputs = document.querySelectorAll(
+  ".input-material input,.input-material textarea"
+);
+
+for (const input of inputs) {
+  input.addEventListener("input", () => {
+    input.setAttribute("value", input.value);
+  });
+}
